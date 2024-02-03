9d60a6844fd97170ec8027c065e4178c86141c6e
balusena <balusena12@gmail.com>
2024-02-03 11:44:22 +0000

    roboshop jenkins files

--- main.tf
+++ main.tf
@@ -45,4 +45,17 @@ resource "aws_elasticache_replication_group" "main" {
   engine_version             = var.engine_version
 }
 
+// Added for troubleshooting
+resource "aws_elasticache_cluster" "instance" {
+  cluster_id           = "${var.component}-${var.env}-instance"
+  engine               = var.engine
+  node_type            = var.node_type
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  engine_version       = var.engine_version
+  port                 = 6379
+  security_group_ids   = [aws_security_group.main.id]
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+}
+
 
